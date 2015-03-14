d46a9ec20808b77179b05f90209686d5d3ec69c2
Yousef <askyous@gmail.com>
2015-03-14 04:16:38 -0700

    marketing items controlled by angular

--- js/controllers.js
+++ js/controllers.js
@@ -51,3 +51,26 @@ xerorexApp.controller('carouselCtrl', function ($scope) {
 			buttonText: 'Learn more'			
 		}]
 })
+
+// Marketing controller
+xerorexApp.controller('marketingCtrl', function($scope){
+	$scope.items = [{
+		header: 'Heading',
+		img: 'http://placehold.it/140x140',
+		description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+		btnText: 'View details',
+		href: '#/Marketing'
+	},{
+		header: 'Heading',
+		img: 'http://placehold.it/140x140',
+		description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+		btnText: 'View details',
+		href: '#/Marketing'
+	},{
+		header: 'Heading',
+		img: 'http://placehold.it/140x140',
+		description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+		btnText: 'View details',
+		href: '#/Marketing'
+	}]
+});
