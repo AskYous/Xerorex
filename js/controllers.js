b8a38386070b1e060515565d68ad30e4e71ee2c7
Yousef <askyous@gmail.com>
2015-03-17 19:55:21 -0700

    Featurettes now controlled by AngularJS

--- js/controllers.js
+++ js/controllers.js
@@ -38,13 +38,13 @@ xerorexApp.controller('carouselCtrl', function ($scope) {
 			img: 'http://placehold.it/500x500',
 			btnText: 'Learn more'			
 		},{
-			header: 'First Item',
+			header: 'Second Item',
 			description: 'This is a description',
 			href: '#/Learn More',
 			img: 'http://placehold.it/500x500',			
 			btnText: 'Learn more'			
 		},{
-			header: 'First Item',
+			header: 'Third Item',
 			description: 'This is a description',
 			href: '#/Learn More',
 			img: 'http://placehold.it/500x500',			
@@ -55,22 +55,42 @@ xerorexApp.controller('carouselCtrl', function ($scope) {
 // Marketing controller
 xerorexApp.controller('marketingCtrl', function($scope){
 	$scope.items = [{
-		header: 'Heading',
+		header: 'Heading1',
 		img: 'http://placehold.it/140x140',
 		description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
 		btnText: 'View details',
 		href: '#/Marketing'
 	},{
-		header: 'Heading',
+		header: 'Heading2',
 		img: 'http://placehold.it/140x140',
 		description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
 		btnText: 'View details',
 		href: '#/Marketing'
 	},{
-		header: 'Heading',
+		header: 'Heading3',
 		img: 'http://placehold.it/140x140',
 		description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
 		btnText: 'View details',
 		href: '#/Marketing'
 	}]
+});
+
+//Fueturette Controller
+xerorexApp.controller('featuretteCtrl', function($scope){
+	$scope.featurettes = [{
+		header: 'First featurette heading.',
+		subHeader: "It'll blow your mind.",
+		description: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.',
+		img: 'http://placehold.it/400x400'
+	},{
+		header: "Oh yeah, it's that good.",
+		subHeader: "See for yourself.",
+		description: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.',
+		img: 'http://placehold.it/400x400'
+	},{
+		header: "And lastly, this one.",
+		subHeader: "Checkmate.",
+		description: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.',
+		img: 'http://placehold.it/400x400'
+	},]
 });
