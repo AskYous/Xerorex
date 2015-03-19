0273330363dfa5e3a9472d080202da1b6488da97
Yousef <askyous@gmail.com>
2015-03-19 02:05:50 -0700

    Bolded carousel text and brand font changes, and replaces some text

--- js/controllers.js
+++ js/controllers.js
@@ -32,22 +32,22 @@ xerorexApp.controller('navbarCtrl', function($scope){
 // Carousel controller
 xerorexApp.controller('carouselCtrl', function ($scope) {
 	$scope.carouselItems = [{
-			header: 'First Item',
-			description: 'This is a description',
+			header: 'Watch',
+			description: 'Our goal has always been to make powerful technology more accessible. More relevant. And ultimately, more personal. Apple Watch represents a new chapter in the relationship people have with technology. It’s the most personal product we’ve ever made, because it’s the first one designed to be worn.',
 			href: '#/Learn More',
-			img: 'img/wear.jpg',
+			img: 'img/apple-watch.jpg',
 			btnText: 'Learn more'			
 		},{
-			header: 'Second Item',
-			description: 'This is a description',
+			header: 'Notebooks',
+			description: 'Boots in seconds. Nothing complicated to learn. Comes with your favorite Google apps. Built for everyday use and perfect for sharing with others. Starting at $199.',
 			href: '#/Learn More',
 			img: 'img/chromebook-hp-14.jpg',			
 			btnText: 'Learn more'			
 		},{
-			header: 'Third Item',
-			description: 'This is a description',
+			header: 'Wearables',
+			description: 'Android Wear organizes your information, suggests what you need, and shows it to you before you even ask. Get messages from your friends, meeting notifications, and weather updates at a glance.',
 			href: '#/Learn More',
-			img: 'img/apple-watch.jpg',			
+			img: 'img/wear.jpg',			
 			btnText: 'Learn more'			
 		}]
 })
@@ -55,19 +55,19 @@ xerorexApp.controller('carouselCtrl', function ($scope) {
 // Marketing controller
 xerorexApp.controller('marketingCtrl', function($scope){
 	$scope.items = [{
-		header: 'Heading1',
+		header: 'Notebooks',
 		img: 'img/chromebook-pixel.png',
 		description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
 		btnText: 'View details',
 		href: '#/Marketing'
 	},{
-		header: 'Heading2',
+		header: 'Mobile',
 		img: 'img/nexus-6.png',
 		description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
 		btnText: 'View details',
 		href: '#/Marketing'
 	},{
-		header: 'Heading3',
+		header: 'Tablets',
 		img: 'img/nexus-7.png',
 		description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
 		btnText: 'View details',
