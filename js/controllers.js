c81acabe3177738267497cab1efa213eebca6075
Yousef <askyous@gmail.com>
2015-04-03 17:08:35 -0700

    Added necessary .json files

--- js/controllers.js
+++ js/controllers.js
@@ -48,7 +48,7 @@ xerorexApp.controller('carouselCtrl', function ($scope) {
 			href: '#/Learn More',
 			img: 'img/wear.jpg',			
 			btnText: 'Learn more'			
-		}]
+		}];
 })
 
 // Marketing controller
@@ -71,7 +71,7 @@ xerorexApp.controller('marketingCtrl', function($scope){
 		description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
 		btnText: 'View details',
 		href: '#/Marketing'
-	}]
+	}];
 });
 
 //Fueturette Controller
@@ -91,5 +91,5 @@ xerorexApp.controller('featuretteCtrl', function($scope){
 		subHeader: "Checkmate.",
 		description: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.',
 		img: 'img/chromebook-pixel.png'
-	}]
+	}];
 });
