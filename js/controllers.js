b2ea647d17eb69eae9f7fcbd635306077dffaf08
Yousef <askyous@gmail.com>
2015-04-04 08:23:45 -0700

    featurettes now loaded through their own JSON file.

--- js/controllers.js
+++ js/controllers.js
@@ -22,21 +22,8 @@ xerorexApp.controller('marketingCtrl', function($scope, $http){
 });
 
 //Fueturette Controller
-xerorexApp.controller('featuretteCtrl', function($scope){
-	$scope.featurettes = [{
-		header: 'Say hello to the new thermostat.',
-		subHeader: "Goodbye old devices.",
-		description: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.',
-		img: 'img/nest.png'
-	},{
-		header: "Oh yeah, it's that good.",
-		subHeader: "See for yourself.",
-		description: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.',
-		img: 'img/nexus-6.png'
-	},{
-		header: "And lastly, this one.",
-		subHeader: "Checkmate.",
-		description: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.',
-		img: 'img/chromebook-pixel.png'
-	}];
+xerorexApp.controller('featuretteCtrl', function($scope, $http){
+	$http.get('data/featurette-items.json').success(function (data) {
+		$scope.featuretteItems = data;
+	});
 });
