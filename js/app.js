2a2ee75517b2beb000cd03295ff919e9ec319c43
Yousef <askyous@gmail.com>
2015-04-07 20:22:27 -0700

    Adding and fixed more routing.

--- js/app.js
+++ js/app.js
@@ -7,16 +7,18 @@ var xerorexApp = angular.module('xerorexApp', [
 xerorexApp.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider.when('/home', {
 		templateUrl: 'partials/homepage/index.html',
+	}).when('/', {
+		templateUrl: 'partials/homepage/index.html',
+	}).when('/#', {
+		templateUrl: 'partials/homepage/index.html',
+	}).when('/Phones', {
+		templateUrl: 'phones/index.html', //for future
+		controller: 'PhoneCtrl'
+	}).when('/TVs', {
+		templateUrl: 'tvs/index.html', //for future
+		controller: 'TvCtrl'
 	})
-	// .when('/Phones', {
-	// 	templateUrl: 'phones/index.html', //for future
-	// 	controller: 'PhoneCtrl'
-	// }).when('/TVs', {
-	// 	templateUrl: 'tvs/index.html', //for future
-	// 	controller: 'TvCtrl'
-	// })
 	.otherwise({
-		redirectTo: '/404'
-	})
-	;	
+		redirectTo: '/404' //URL doesn't exist
+	});	
 }]);
