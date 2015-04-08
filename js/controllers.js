83ab85f2b5a07e5daa8e08cfca29d192c4687c0b
Yousef <askyous@gmail.com>
2015-04-07 19:44:30 -0700

    moved controllers to correct angular module

--- js/controllers.js
+++ js/controllers.js
@@ -1,28 +1,28 @@
 var xerorexControllers = angular.module('xerorexControllers', []);
 
 // Navbar Controller
-xerorexApp.controller('navbarCtrl', function ($scope, $http){
+xerorexControllers.controller('navbarCtrl', function ($scope, $http){
 	$http.get('data/menu-items.json').success(function (data) {
 		$scope.menuItems = data;
 	});
 });
 
 // Carousel controller
-xerorexApp.controller('carouselCtrl', function ($scope, $http) {
+xerorexControllers.controller('carouselCtrl', function ($scope, $http) {
 	$http.get('data/carousel-items.json').success(function (data) {
 		$scope.carouselItems = data;
 	});
 })
 
 // Marketing controller
-xerorexApp.controller('marketingCtrl', function($scope, $http){
+xerorexControllers.controller('marketingCtrl', function($scope, $http){
 	$http.get('data/marketing-items.json').success(function (data) {
 		$scope.marketingItems = data;
 	});
 });
 
 //Fueturette Controller
-xerorexApp.controller('featuretteCtrl', function($scope, $http){
+xerorexControllers.controller('featuretteCtrl', function($scope, $http){
 	$http.get('data/featurette-items.json').success(function (data) {
 		$scope.featuretteItems = data;
 	});
