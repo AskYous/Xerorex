b07be10819f83653a8a0d940364793b9e064d531
Yousef <askyous@gmail.com>
2015-05-16 13:25:46 -0700

    Organizing data into folders

--- js/controllers.js
+++ js/controllers.js
@@ -2,28 +2,28 @@ var xerorexControllers = angular.module('xerorexControllers', []);
 
 // Navbar Controller
 xerorexControllers.controller('navbarCtrl', function ($scope, $http){
-	$http.get('data/menu-items.json').success(function (data) {
+	$http.get('data/homepage/menu-items.json').success(function (data) {
 		$scope.menuItems = data;
 	});
 });
 
 // Carousel controller
 xerorexControllers.controller('carouselCtrl', function ($scope, $http) {
-	$http.get('data/carousel-items.json').success(function (data) {
+	$http.get('data/homepage/carousel-items.json').success(function (data) {
 		$scope.carouselItems = data;
 	});
 })
 
 // Marketing controller
 xerorexControllers.controller('marketingCtrl', function($scope, $http){
-	$http.get('data/marketing-items.json').success(function (data) {
+	$http.get('data/homepage/marketing-items.json').success(function (data) {
 		$scope.marketingItems = data;
 	});
 });
 
 //Fueturette Controller
 xerorexControllers.controller('featuretteCtrl', function($scope, $http){
-	$http.get('data/featurette-items.json').success(function (data) {
+	$http.get('data/homepage/featurette-items.json').success(function (data) {
 		$scope.featuretteItems = data;
 	});
 });
