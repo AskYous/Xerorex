d5f9dada092e837d4891a39b8025bf621d0c46c7
Yousef Shanawany <askyous@gmail.com>
2015-10-03 07:48:01 -0700

    Changed version numbers to referenced files

--- public/js/directives.js
+++ public/js/directives.js
@@ -2,16 +2,16 @@ var xerorexDirectives = angular.module('xerorexDirectives', []);
 
 xerorexControllers.directive('carousel', function () {
   return {
-    templateUrl: 'public/html/shared/carousel.html'
+    templateUrl: 'public/html/shared/carousel.html?v=1'
   }
 });
 xerorexControllers.directive('featurettes', function () {
   return {
-    templateUrl: 'public/html/shared/featurettes.html'
+    templateUrl: 'public/html/shared/featurettes.html?v=1'
   }
 });
 xerorexControllers.directive('marketing', function () {
   return {
-    templateUrl: 'public/html/shared/marketing.html'
+    templateUrl: 'public/html/shared/marketing.html?v=1'
   }
 });
